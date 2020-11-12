dfd565305bebf2943018ee526c11c219f307360e
Squarerootofpi <53194917+Squarerootofpi@users.noreply.github.com>
2020-11-12 09:53:24 -0700

    Added templating for root

--- app_restaurant/views.py
+++ app_restaurant/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def indexPageView(request) :
-    sOutput = '<html><head><title>My Title</title></head><body><p style="color:red;"><b>one</b></p><p style="color:blue;">two</p><p style="font-size:50px;">three</p><ul><li>A</li><li>B</li><li>C</li></ul></body></html>'
-    return HttpResponse(sOutput)
+    # sOutput = '<html><head><title>My Title</title></head><body><p style="color:red;"><b>one</b></p><p style="color:blue;">two</p><p style="font-size:50px;">three</p><ul><li>A</li><li>B</li><li>C</li></ul></body></html>'
+    # return HttpResponse(sOutput)
+    return render(request, 'app_restaurant_templates/index.html')
 
 def getAll(request):
     """
