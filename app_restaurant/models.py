01a109eb97aa42abaedc1f65e02587e408bb3791
Squarerootofpi <53194917+Squarerootofpi@users.noreply.github.com>
2020-11-12 15:39:28 -0700

    Added models for the restaurants
    Must be connected to postgresdatabase for it to work..

--- app_restaurant/models.py
+++ app_restaurant/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from enum import Enum
+
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,13 +10,40 @@ class Restaurant(models.Model):
     """A typical class defining a model, derived from the Model class."""
 
     # Fields
-    my_field_name = models.CharField(max_length=20, help_text='Enter field documentation')
-    ...
+    
+    name = models.CharField(max_length=128)
+    link = models.URLField('Web Address', blank=True)
+    class COST(Enum):
+        cheap = ('cheap', 'Cheap')
+        moderate = ('moderate', 'moderate')
+        expensive = ('expensive', 'Expensive')
+
+        @classmethod
+        def get_value(cls, member):
+            return member.value[0]
+    cost = models.CharField(
+        max_length=32,
+        choices=[x.value for x in COST],
+        default=COST.get_value(COST.moderate),  # OR STATUS.available[0]
+    )
+    class CUISINE(Enum):
+        chinese =('chinese', 'Chinese')
+        american = ('american', 'American')
+        other = ('other', 'Other Cousine')
+
+        @classmethod
+        def get_value(cls, member):
+            return member.value[0]
+    cuisine = models.CharField(
+        max_length=32,
+        choices=[x.value for x in CUISINE],
+        default=CUISINE.get_value(CUISINE.american),  # OR STATUS.available[0]
+    )
 
     # Metadata
-    class Meta: 
-        ordering = ['-my_field_name']
 
+    class Meta: 
+        ordering = ['name']
     # Methods
     def get_absolute_url(self):
         """Returns the url to access a particular instance of MyModelName."""
@@ -21,5 +51,5 @@ class Restaurant(models.Model):
     
     def __str__(self):
         """String for representing the MyModelName object (in Admin site etc.)."""
-        return self.my_field_name
-
+        return self.name
+    # manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
